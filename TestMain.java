6e1149a9a0388c790cdc3941ace57cda628820f5
Esmeralda Soto <40704347+eesoto@users.noreply.github.com>
2025-06-02 11:11:36 -0500

    Create TestMain.java

---
+++ TestMain.java
@@ -0,0 +1,15 @@
+public class TestMain {
+  public static void test1(){
+    int howmany = 10;
+    int ans = Main.adder(howmany);
+    boolean ok = and == 15;
+    System.out.println(ok);
+
+  boolean[] testlots = { Main.adder(howmuch:30) ==35};
+    System.out.println(Arrays.toString(testlots));
+  }
+  public static void main(String[] args){
+    System.out.println(adder(howmuch:50));
+    System.out.println("YES!!!");
+  }
+}
